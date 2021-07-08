100e74ad0a28217860b3c89e5f9c432a038e1787
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2021-07-08 13:22:55 +0200

    use Box for transferring args to child processes

--- src/init.rs
+++ src/init.rs
@@ -9,7 +9,7 @@ use std::{
 use capctl::prctl;
 use nix::sched::{self, CloneFlags};
 use nix::sys::socket;
-use nix::unistd::{Pid, Uid};
+use nix::unistd::{Gid, Pid, Uid};
 use oci_spec::runtime::{self, Spec};
 
 #[derive(Clone, Copy, Debug, Default)]
@@ -40,19 +40,19 @@ struct InitConfig {
 }
 
 #[derive(Debug)]
-struct SetupArgs<'a> {
+struct SetupArgs {
 	stage: InitStage,
 	init_pipe: RawFd,
 	parent_child_sync: SocketPair,
 	parent_grandchild_sync: SocketPair,
-	config: &'a InitConfig,
+	config: InitConfig,
 }
 
 #[repr(align(16))]
-struct CloneArgs<'a> {
+struct CloneArgs {
 	stack: [u8; 4096],
-	args: &'a SetupArgs<'a>,
-	child_func: Box<dyn FnMut(&SetupArgs) -> isize + 'a>,
+	args: SetupArgs,
+	child_func: Box<dyn Fn(SetupArgs) -> isize>,
 }
 
 pub fn init_container() {
@@ -62,7 +62,7 @@ pub fn init_container() {
 	// During this process, it:
 	// - forks a child process
 	// - unshares from the user namespaces
-	// - unshares from all other requested namespace
+	// - unshares from all other requested namespaces
 	// - creates a grandchild process in a new PID namespace
 	// - reports back the child- and grandchild-PID to the create process
 	// - Waits for the exec-fifo to open during the runh start call
@@ -79,10 +79,10 @@ pub fn init_container() {
 		.parse()
 		.expect("RUNH_SPEC_FILE was not an integer!");
 	let spec_file = unsafe { File::from_raw_fd(RawFd::from(spec_fd)) };
-	let config: Spec = serde_json::from_reader(&spec_file).expect("Unable to read spec file!");
+	let spec: Spec = serde_json::from_reader(&spec_file).expect("Unable to read spec file!");
 
 	debug!("generate clone-flags");
-	let cloneflags = if let Some(namespaces) = &config.linux.as_ref().unwrap().namespaces {
+	let cloneflags = if let Some(namespaces) = &spec.linux.as_ref().unwrap().namespaces {
 		generate_cloneflags(namespaces)
 	} else {
 		CloneFlags::empty()
@@ -114,21 +114,21 @@ pub fn init_container() {
 	);
 
 	debug!("jump into init_stage");
-	init_stage(&SetupArgs {
+	init_stage(SetupArgs {
 		stage: InitStage::PARENT,
 		init_pipe: init_pipe.into_raw_fd(),
 		parent_child_sync,
 		parent_grandchild_sync,
-		config: &InitConfig {
-			spec: config,
+		config: InitConfig {
+			spec,
 			cloneflags,
 		},
 	});
 }
 
-fn clone_process(mut args: CloneArgs) -> nix::unistd::Pid {
+fn clone_process(mut args: Box<CloneArgs>) -> nix::unistd::Pid {
 	extern "C" fn callback(data: *mut CloneArgs) -> i32 {
-		let cb: &mut CloneArgs = unsafe { &mut *data };
+		let cb: Box<CloneArgs> = unsafe { Box::from_raw(data) };
 		(*cb.child_func)(cb.args) as i32
 	}
 
@@ -140,7 +140,7 @@ fn clone_process(mut args: CloneArgs) -> nix::unistd::Pid {
 			std::mem::transmute(callback as extern "C" fn(*mut CloneArgs) -> i32),
 			ptr_aligned as *mut libc::c_void,
 			combined,
-			&mut args as *mut _ as *mut libc::c_void,
+			Box::into_raw(args) as *mut _ as *mut libc::c_void,
 		)
 	};
 
@@ -149,15 +149,15 @@ fn clone_process(mut args: CloneArgs) -> nix::unistd::Pid {
 		.expect("Could not clone parent process!")
 }
 
-fn init_stage(args: &SetupArgs) -> isize {
+fn init_stage(args: SetupArgs) -> isize {
 	match args.stage {
 		InitStage::PARENT => {
 			debug!("enter init_stage parent");
 			// Setting the name is just for debugging purposes so it doesnt cause problems if it fails
 			let _ = prctl::set_name("runh:PARENT");
-			let child_pid = clone_process(CloneArgs {
+			let child_pid = clone_process(Box::new(CloneArgs {
 				stack: [0; 4096],
-				args: &SetupArgs {
+				args: SetupArgs {
 					stage: InitStage::CHILD,
 					init_pipe: args.init_pipe,
 					parent_child_sync: args.parent_child_sync,
@@ -165,7 +165,7 @@ fn init_stage(args: &SetupArgs) -> isize {
 					config: args.config,
 				},
 				child_func: Box::new(init_stage),
-			});
+			}));
 			debug!("Created child with pid {}", child_pid);
 			debug!("Wait for synchronization with children!");
 
@@ -197,6 +197,7 @@ fn init_stage(args: &SetupArgs) -> isize {
 			}
 
 			//TODO: Unshare user namespace if requested
+			//TODO: Let parent setup uidmap/gidmap if a user namespace was joined
 
 			nix::unistd::setresuid(Uid::from_raw(0), Uid::from_raw(0), Uid::from_raw(0))
 				.expect("could not become root in user namespace!");
@@ -211,9 +212,9 @@ fn init_stage(args: &SetupArgs) -> isize {
 			nix::sched::unshare(flags).expect("could not unshare non-user namespaces!");
 
 			// Fork again into new PID-Namespace and send PID to parent
-			let grandchild_pid: i32 = clone_process(CloneArgs {
+			let grandchild_pid: i32 = clone_process(Box::new(CloneArgs {
 				stack: [0; 4096],
-				args: &SetupArgs {
+				args: SetupArgs {
 					stage: InitStage::GRANDCHILD,
 					init_pipe: args.init_pipe,
 					parent_child_sync: args.parent_child_sync,
@@ -221,12 +222,11 @@ fn init_stage(args: &SetupArgs) -> isize {
 					config: args.config,
 				},
 				child_func: Box::new(init_stage),
-			})
+			}))
 			.into();
 
 			// Send grandchild-PID to parent process
 			debug!("writing PID to fd {}", args.parent_child_sync.child);
-
 			let mut child_sync_pipe = unsafe { File::from_raw_fd(args.parent_child_sync.child) };
 			let written_bytes = child_sync_pipe
 				.write(&grandchild_pid.to_le_bytes())
@@ -236,7 +236,22 @@ fn init_stage(args: &SetupArgs) -> isize {
 		}
 		InitStage::GRANDCHILD => {
 			debug!("enter init_stage grandchild");
+			let _ = prctl::set_name("runh:INIT");
 			debug!("Welcome to the container! This is PID {}", Pid::this());
+
+			// Set SID, UID, GID
+			let _ = nix::unistd::setsid().expect("Could not set session ID");
+			nix::unistd::setuid(Uid::from_raw(0)).expect("Could not set user ID");
+			nix::unistd::setgid(Gid::from_raw(0)).expect("Could not set group ID");
+
+			// TODO: Call setgroups if !is_rootless_euid && is_setgroup (?)
+
+			// Unshare Cgroup namespace if requested to
+			if args.config.cloneflags.contains(CloneFlags::CLONE_NEWCGROUP) {
+				// TODO: Synchronize with runh create for cgroup setup
+				nix::sched::unshare(CloneFlags::CLONE_NEWCGROUP)
+					.expect("could not unshare cgroups namespace!");
+			}
 			loop {}
 		}
 	}
@@ -269,15 +284,7 @@ fn join_namespaces(namespaces: &Vec<runtime::LinuxNamespace>) {
 
 	for ns_config in &configured_ns {
 		debug!("joining namespace {:?}", ns_config.1);
-		let flags = match ns_config.1.typ {
-			runtime::LinuxNamespaceType::cgroup => CloneFlags::CLONE_NEWCGROUP,
-			runtime::LinuxNamespaceType::ipc => CloneFlags::CLONE_NEWIPC,
-			runtime::LinuxNamespaceType::mount => CloneFlags::CLONE_NEWNS,
-			runtime::LinuxNamespaceType::network => CloneFlags::CLONE_NEWNET,
-			runtime::LinuxNamespaceType::pid => CloneFlags::CLONE_NEWPID,
-			runtime::LinuxNamespaceType::user => CloneFlags::CLONE_NEWUSER,
-			runtime::LinuxNamespaceType::uts => CloneFlags::CLONE_NEWUTS,
-		};
+		let flags = get_cloneflag(ns_config.1.typ);
 		nix::sched::setns(ns_config.0.as_raw_fd(), flags)
 			.expect(format!("Failed to join NS {:?}", ns_config.1).as_str());
 	}
@@ -286,16 +293,19 @@ fn join_namespaces(namespaces: &Vec<runtime::LinuxNamespace>) {
 fn generate_cloneflags(namespaces: &Vec<runtime::LinuxNamespace>) -> CloneFlags {
 	let mut result = CloneFlags::empty();
 	for ns in namespaces {
-		let flag = match ns.typ {
-			runtime::LinuxNamespaceType::cgroup => CloneFlags::CLONE_NEWCGROUP,
-			runtime::LinuxNamespaceType::ipc => CloneFlags::CLONE_NEWIPC,
-			runtime::LinuxNamespaceType::mount => CloneFlags::CLONE_NEWNS,
-			runtime::LinuxNamespaceType::network => CloneFlags::CLONE_NEWNET,
-			runtime::LinuxNamespaceType::pid => CloneFlags::CLONE_NEWPID,
-			runtime::LinuxNamespaceType::user => CloneFlags::CLONE_NEWUSER,
-			runtime::LinuxNamespaceType::uts => CloneFlags::CLONE_NEWUTS,
-		};
-		result.insert(flag);
+		result.insert(get_cloneflag(ns.typ));
 	}
 	return result;
+}
+
+fn get_cloneflag(typ: runtime::LinuxNamespaceType) -> CloneFlags {
+	match typ {
+		runtime::LinuxNamespaceType::cgroup => CloneFlags::CLONE_NEWCGROUP,
+		runtime::LinuxNamespaceType::ipc => CloneFlags::CLONE_NEWIPC,
+		runtime::LinuxNamespaceType::mount => CloneFlags::CLONE_NEWNS,
+		runtime::LinuxNamespaceType::network => CloneFlags::CLONE_NEWNET,
+		runtime::LinuxNamespaceType::pid => CloneFlags::CLONE_NEWPID,
+		runtime::LinuxNamespaceType::user => CloneFlags::CLONE_NEWUSER,
+		runtime::LinuxNamespaceType::uts => CloneFlags::CLONE_NEWUTS,
+	}
 }
