5c5b28ba2c44eeafbc33729ef48e3a92661b2e91
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2021-10-06 16:44:39 +0200

    add force argument for cri-o

--- src/main.rs
+++ src/main.rs
@@ -204,6 +204,14 @@ pub fn main() {
 						.takes_value(true)
 						.required(true)
 						.help("Id of the container"),
+				)
+				.arg(
+					Arg::with_name("FORCE")
+						.long("force")
+						.short("f")
+						.takes_value(false)
+						.required(false)
+						.help("Currently unimplemented!"),
 				),
 		)
 		.subcommand(
