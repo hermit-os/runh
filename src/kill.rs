0212b7bd90ab09548b97dff214f547ca9a468473
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2021-10-15 15:38:43 +0200

    allow signal numbers in kill command

--- src/kill.rs
+++ src/kill.rs
@@ -1,9 +1,9 @@
 use nix::unistd::Pid;
-use std::{path::PathBuf, str::FromStr};
+use std::{convert::TryFrom, path::PathBuf, str::FromStr};
 
 use crate::state;
 
-pub fn kill_container(project_dir: PathBuf, id: Option<&str>, signal_str: Option<&str>, all: bool) {
+pub fn kill_container(project_dir: PathBuf, id: Option<&str>, sig: Option<&str>, all: bool) {
 	let container_state = state::get_container_state(project_dir, id.unwrap());
 	if container_state.status != "created" && container_state.status != "running" {
 		panic!("Cannot send signals to non-running containers!")
@@ -14,20 +14,23 @@ pub fn kill_container(project_dir: PathBuf, id: Option<&str>, signal_str: Option
 	}
 
 	let pid = container_state.pid.unwrap();
-	let signal = if !signal_str.unwrap().starts_with("SIG") {
-		"SIG".to_owned() + signal_str.unwrap()
+	let signal = if let Ok(sig_nr) = sig.unwrap().parse::<i32>() {
+		nix::sys::signal::Signal::try_from(sig_nr)
+			.expect(format!("Could not parse signal number {}", sig.unwrap()).as_str())
 	} else {
-		signal_str.unwrap().to_owned()
+		let signal_str = if !sig.unwrap().starts_with("SIG") {
+			"SIG".to_owned() + sig.unwrap()
+		} else {
+			sig.unwrap().to_owned()
+		};
+		nix::sys::signal::Signal::from_str(signal_str.as_str())
+			.expect(format!("Could not parse signal string {}", sig.unwrap()).as_str())
 	};
-	nix::sys::signal::kill(
-		Pid::from_raw(pid),
-		nix::sys::signal::Signal::from_str(signal.as_str())
-			.expect(format!("Could not parse signal string {}", signal_str.unwrap()).as_str()),
-	)
-	.expect(
+
+	nix::sys::signal::kill(Pid::from_raw(pid), signal).expect(
 		format!(
 			"Could not send signal {} to container process ID  {}!",
-			signal_str.unwrap(),
+			sig.unwrap(),
 			pid
 		)
 		.as_str(),
