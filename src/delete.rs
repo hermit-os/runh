f9b3b21b707bde988fec6f40275bca9814dedc2e
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2021-11-26 10:37:34 +0100

    Ignore requests for deleting non-existing containers

--- src/delete.rs
+++ src/delete.rs
@@ -29,43 +29,22 @@ fn reset_network_namespace(container_dir: &PathBuf) -> Result<(), Box<dyn std::e
 }
 
 pub fn delete_container(mut project_dir: PathBuf, id: Option<&str>, force: bool) {
-	let container_state = state::get_container_state(project_dir.clone(), id.unwrap());
-	if container_state.status != "stopped" {
-		if !force {
-			panic!("Tried to delete a container that is not stopped!");
-		} else {
-			if container_state.status != "creating" {
-				warn!("Container is still running. Force-deleting...");
-				kill::kill_container(project_dir.clone(), id, Some("SIGKILL"), false);
+	if let Some(container_state) = state::get_container_state(project_dir.clone(), id.unwrap()) {
+		if container_state.status != "stopped" {
+			if !force {
+				panic!("Tried to delete a container that is not stopped!");
 			} else {
-				warn!("Container has not finished creation. Force-deleting...")
+				if container_state.status != "creating" {
+					warn!("Container is still running. Force-deleting...");
+					kill::kill_container(project_dir.clone(), id, Some("SIGKILL"), false);
+				} else {
+					warn!("Container has not finished creation. Force-deleting...")
+				}
 			}
 		}
-	}
 
-	let mut delete_file = false;
-	project_dir.push(id.unwrap());
-	// path to the container specification
-	let container_dir = project_dir.clone();
-	project_dir.push("container.json");
+		let container_dir = project_dir.join(id.unwrap());
 
-	if let Ok(mut file) = fs::OpenOptions::new()
-		.read(true)
-		.write(false)
-		.open(project_dir)
-	{
-		let mut contents = String::new();
-		file.read_to_string(&mut contents)
-			.expect("Unable to read container specification");
-
-		if let Ok(_container) = serde_json::from_str::<OCIContainer>(&contents) {
-			delete_file = true;
-		}
-	} else {
-		println!("Container `{}` doesn't exists", id.unwrap());
-	}
-
-	if delete_file {
 		let rootfs_overlay_dir = container_dir.join("rootfs/merged");
 		if rootfs_overlay_dir.exists() {
 			nix::mount::umount2(&rootfs_overlay_dir, MntFlags::MNT_DETACH)
@@ -79,8 +58,11 @@ pub fn delete_container(mut project_dir: PathBuf, id: Option<&str>, force: bool)
 
 		// delete all temporary files
 		fs::remove_dir_all(container_dir).expect("Unable to delete container");
-	}
 
-	//Additionally to deleting all the files, we should also delete all remaining processes spawned by the container init process.
-	//However, without cgroup support there is currently no real way to do this as we do not know when (and how) the init process will be killed
+		//Additionally to deleting all the files, we should also delete all remaining processes spawned by the container init process.
+		//However, without cgroup support there is currently no real way to do this as we do not know when (and how) the init process will be killed
+
+	} else {
+		warn!("Container `{}` doesn't exists! Skipping deletion...", id.unwrap());
+	}
 }
