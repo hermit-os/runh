cdb047f2f5b1422b7c40d8e1097d0d2c4614f29c
Jonas Schroeder <jonas.schroeder1@rwth-aachen.de>
2022-02-16 13:22:32 +0100

    require container args in spec command

--- src/spec.rs
+++ src/spec.rs
@@ -2,12 +2,20 @@ use oci_spec::runtime;
 use std::fs;
 use std::path;
 
-pub fn create_spec(bundle: Option<&str>) {
+pub fn create_spec(bundle: Option<&str>, args: Vec<String>) {
 	let dir = fs::canonicalize(path::PathBuf::from(bundle.unwrap()))
 		.expect("Unable to determine absolute bundle path");
 	let mut config_file = dir.clone();
 	config_file.push("config.json");
-	let spec: runtime::Spec = Default::default();
+	let spec: runtime::Spec = runtime::SpecBuilder::default()
+		.process(
+			runtime::ProcessBuilder::default()
+				.args(args)
+				.build()
+				.unwrap(),
+		)
+		.build()
+		.unwrap();
 	spec.save(&config_file.to_str().unwrap())
 		.expect("Unable to write new specification file");
 }
