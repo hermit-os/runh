1ecda0ea8c31680ebcfcffb6f6ffba91ccd819a9
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2023-09-27 17:39:20 +0000

    allow write access to the rootfs
    - this required to create a unix domain socket between virtiofsd and qemu

--- src/spec.rs
+++ src/spec.rs
@@ -4,6 +4,8 @@ use std::path;
 pub fn create_spec(bundle: path::PathBuf, args: Vec<String>) {
 	let mut config_file = bundle;
 	config_file.push("config.json");
+	let mut root = runtime::Root::default();
+	root.set_readonly(false.into());
 	let spec: runtime::Spec = runtime::SpecBuilder::default()
 		.process(
 			runtime::ProcessBuilder::default()
@@ -11,6 +13,7 @@ pub fn create_spec(bundle: path::PathBuf, args: Vec<String>) {
 				.build()
 				.unwrap(),
 		)
+		.root(root)
 		.build()
 		.unwrap();
 	spec.save(config_file.to_str().unwrap())
